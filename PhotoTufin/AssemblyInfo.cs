e918c58f9beb09eb5033783b333d6d654e9643be
Holger Maerz <holger@nakade.de>
2022-09-09 09:15:25 +0200

    init

---
+++ PhotoTufin/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Windows;
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
+    //(used if a resource is not found in the page,
+    // or application resource dictionaries)
+    ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
+    //(used if a resource is not found in the page,
+    // app, or any theme specific resource dictionaries)
+)]
+
+[assembly: AssemblyDescription ( "Tool für das Finden doppelter Photos" )]
+[assembly: AssemblyCopyright ( "Copyright © Dr. Holger Maerz 2022" )]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PhotoTufin/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Reflection;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PhotoTufin
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Title = AppName;
+            AppVersion.Text = $"v{VersionShort}";
+        }
+
+        /// <summary>
+        /// Gibt die Versionsnummer der Assembly zurück.
+        /// </summary>
+        /// <returns>string</returns>
+        private static string Version
+        {
+            get
+            {
+                var version = Assembly.GetExecutingAssembly().GetName().Version;
+                return version != null ? $"{version}" : "1.0";
+            }
+        }
+        
+        /// <summary>
+        /// Gibt die Versionsnummer der Assembly zurück.
+        /// </summary>
+        /// <returns>string</returns>
+        private static string VersionShort
+        {
+            get
+            {
+                var version = Assembly.GetExecutingAssembly().GetName().Version;
+                return version != null ? $"{version.Major}.{version.Minor}" : "1.0";
+            }
+        }
+        
+        /// <summary>
+        /// Gibt die Versionsnummer der Assembly zurück.
+        /// </summary>
+        /// <returns>string</returns>
+        private static string AppName
+        {
+            get
+            {
+                var name = Assembly.GetExecutingAssembly().GetName().Name;
+                return name != null ? $"{name}" : "MyApp";
+            }
+        }
+    }
+}
